4b22c0476e969d0c293db24aadaf29566feaa025
dzozrawr <jovan_96@live.ru>
2022-07-22 17:20:06 +0200

    added dialogue switching based on the chosen player/girl

--- Assets/_Game/Scripts/GameController.cs
+++ Assets/_Game/Scripts/GameController.cs
@@ -5,6 +5,21 @@ using Cinemachine;
 
 public class GameController : MonoBehaviour
 {
+    private static GameController instance=null;
+    public static GameController Instance { get => instance; }
+
+    public delegate void ConversationChangeHandler(string conversationName);
+    public event ConversationChangeHandler OnConversationChanged;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +29,30 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+#if UNITY_EDITOR
         if (Input.GetKeyDown(KeyCode.Space))
         {
             StartContestantsPhase();
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SetConversation("Amelia - Baking");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SetConversation("Olivia - Dogs");
+        }
+#endif
+
+
+
+    }
+
+    public void SetConversation(string conversationName)
+    {
+        OnConversationChanged?.Invoke(conversationName);
     }
 
     public void StartPlayerPicking()
