aa498bfef8a9097b3d22ae5fc436fe13eca7653e
dzozrawr <jovan_96@live.ru>
2022-07-27 16:42:43 +0200

    finished player walking to contestants, fixed dog mini game buttons

--- Assets/_Game/Scripts/CameraFollowTrigger.cs
+++ Assets/_Game/Scripts/CameraFollowTrigger.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation.Examples;
+using PathCreation.Utility;
+using PathCreationEditor;
+using PathCreation;
 
 public class CameraFollowTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PathFollower player = null;
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player.speed = 3;//set the speed
+      //  player.pathCreator = path;//put player on another path
+       
+
+        Debug.Log("OnTriggerEnter");
+    }
 }
--- Assets/_Game/Scripts/DogMiniGame/DogCommandButton.cs
+++ Assets/_Game/Scripts/DogMiniGame/DogCommandButton.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DogMiniGame;
+using UnityEngine.UI;
+
 public class DogCommandButton : MonoBehaviour
 {
     public DogCommand dogCommand;
+
+    private Button button;
     // Start is called before the first frame update
     void Start()
     {
-        
+       // button.de
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
