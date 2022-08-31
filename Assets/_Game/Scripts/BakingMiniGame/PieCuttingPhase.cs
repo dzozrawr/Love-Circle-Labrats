246af20b41532a4bf70eda9eebaac48a64849354
dzozrawr <jovan_96@live.ru>
2022-08-31 09:57:55 +0200

    finished the skelet of pie cutting phase in the baking mini game

--- Assets/_Game/Scripts/BakingMiniGame/PieCuttingPhase.cs
+++ Assets/_Game/Scripts/BakingMiniGame/PieCuttingPhase.cs
@@ -8,50 +8,54 @@ public class PieCuttingPhase : BakingMiniGameState
     private BakingMiniGameCanvas bakingMiniGameCanvas;
     private PieDish pieDish;
 
-    private GameObject pieCutter=null;
-    private GameObject pieTopLayerCut=null;
+    private GameObject pieCutter = null;
+    private GameObject pieTopLayerCut = null;
 
-    private bool isPhaseDone=false;
+    private bool isPhaseDone = false;
 
     private Vector3 pieCutterInitPos;
     public void InitState(BakingMiniGame bmg)
     {
-        bakingMiniGameCanvas=bmg.bakingMiniGameCanvas;
-        pieDish=bmg.PieDish;
+        bakingMiniGameCanvas = bmg.bakingMiniGameCanvas;
+        pieDish = bmg.PieDish;
 
         bmg.bakingMiniGameCanvas.pieCuttingUIElementsGroup.SetActive(true);
 
     }
     public BakingMiniGameState DoState(BakingMiniGame bmg)
     {
-        if(isPhaseDone){
+        if (isPhaseDone)
+        {
             return this;
         }
-        if(bakingMiniGameCanvas.ChosenPatternNumber==-1) return this;
+        if (bakingMiniGameCanvas.ChosenPatternNumber == -1) return this;
 
-        pieCutter=pieDish.pieCutters[bakingMiniGameCanvas.ChosenPatternNumber];
-        pieTopLayerCut=pieDish.pieTopLayers[bakingMiniGameCanvas.ChosenPatternNumber];
-        bakingMiniGameCanvas.ChosenPatternNumber=-1;
+        pieCutter = pieDish.pieCutters[bakingMiniGameCanvas.ChosenPatternNumber];
+        pieTopLayerCut = pieDish.pieTopLayers[bakingMiniGameCanvas.ChosenPatternNumber];
+        bakingMiniGameCanvas.ChosenPatternNumber = -1;
 
-        pieCutterInitPos=pieCutter.transform.position;
-        pieCutter.transform.position=pieDish.pieCutterAbovePos.position;
+        pieCutterInitPos = pieCutter.transform.position;
+        pieCutter.transform.position = pieDish.pieCutterAbovePos.position;
         pieCutter.gameObject.SetActive(true);
-        
-        pieCutter.transform.DOMove(pieCutterInitPos,1f).OnComplete(()=>{
-            bmg.Invoke(nameof(MoveCutterUpWDelay),0.5f);
+
+        pieCutter.transform.DOMove(pieCutterInitPos, 1f).OnComplete(() =>
+        {
+            bmg.StartCoroutine(MoveCutterUpWDelay(0.5f));
         });
-
-
 
         return this;
     }
 
-    private void MoveCutterUpWDelay(){
+    IEnumerator MoveCutterUpWDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
         pieDish.topLayer.SetActive(false);
         pieTopLayerCut.SetActive(true);
 
-         pieCutter.transform.DOMove(pieDish.pieCutterAbovePos.position,1f).OnComplete(()=>{
-            isPhaseDone=true;
+        pieCutter.transform.DOMove(pieDish.pieCutterAbovePos.position, 1f).OnComplete(() =>
+        {
+            isPhaseDone = true;
+            pieCutter.SetActive(false);
         });
     }
 
