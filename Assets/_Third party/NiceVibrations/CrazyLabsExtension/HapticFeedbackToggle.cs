cf6cac9f6d6532fa80fe462c54616583fe55d90b
xsshaddx <ensarsacirovic55@gmail.com>
2022-09-05 14:30:54 +0200

    Added settings switches from Fridge Organizing (prefabs, scripts and textures), added NiceVibrations CrazyLabs asset to _ThirdParty

---
+++ Assets/_Third party/NiceVibrations/CrazyLabsExtension/HapticFeedbackToggle.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace NiceVibrations.CrazyLabsExtension
+{
+    [RequireComponent( typeof( Button ) )]
+    public class HapticFeedbackToggle : MonoBehaviour
+    {
+        private Button _buttonCached;
+
+        [SerializeField]
+        private Image _activeImage;
+
+        [SerializeField]
+        private Sprite _hapticsActiveSprite;
+
+        [SerializeField]
+        private Sprite _hapticsInactiveSprite;
+
+        private void Awake()
+        {
+            _buttonCached = GetComponent<Button>( );
+
+            _buttonCached.onClick.AddListener( ToggleHaptics );
+
+            HapticFeedbackController.OnHapticsEnabledChanged += RefreshButtonStatus;
+        }
+
+        private void OnDestroy()
+        {
+            _buttonCached.onClick.RemoveListener( ToggleHaptics );
+
+            HapticFeedbackController.OnHapticsEnabledChanged -= RefreshButtonStatus;
+        }
+
+        private void Start()
+        {
+            RefreshButtonStatus( HapticFeedbackController.IsHapticsEnabled );
+        }
+
+        private void ToggleHaptics()
+        {
+            HapticFeedbackController.ToggleHaptics( );
+        }
+
+        private void RefreshButtonStatus( bool isHapticsActive )
+        {
+            _activeImage.sprite = isHapticsActive ? _hapticsActiveSprite : _hapticsInactiveSprite;
+        }
+    }
+}
