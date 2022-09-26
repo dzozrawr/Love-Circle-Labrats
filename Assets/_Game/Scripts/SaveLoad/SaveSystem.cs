5e4d1f926341afc17103d87a223a63d87fb5de33
dzozrawr <jovan_96@live.ru>
2022-09-26 16:47:27 +0200

    working on encoding the save data (unstable)

--- Assets/_Game/Scripts/SaveLoad/SaveSystem.cs
+++ Assets/_Game/Scripts/SaveLoad/SaveSystem.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System;
 using System.Threading;
 using System.Runtime.Serialization;
+using System.Xml.Linq;
 
 public static class SaveSystem
 {
@@ -14,40 +15,74 @@ public static class SaveSystem
     public static string fullSavePath = Application.persistentDataPath + "/FridgeOrganizingSaveData.bin";
     public static void SaveGame(SaveData saveData)
     {
-         BinaryFormatter formatter = new BinaryFormatter();
+        /*          BinaryFormatter formatter = new BinaryFormatter();
 
-        FileStream stream = new FileStream(fullSavePath, FileMode.Create);
+                FileStream stream = new FileStream(fullSavePath, FileMode.Create);
 
-        formatter.Serialize(stream, saveData);
-        stream.Close(); 
+                formatter.Serialize(stream, saveData);
+                stream.Close();  */
 
-/*           DataContractSerializer bf = new DataContractSerializer(data.GetType());
+       // FileStream file = File.Create(fullSavePath);
+
+        DataContractSerializer bf = new DataContractSerializer(saveData.GetType());
         MemoryStream streamer = new MemoryStream();
 
         //Serialize the file
         bf.WriteObject(streamer, saveData);
         streamer.Seek(0, SeekOrigin.Begin);
 
+        //bf.ReadObject()
+
+        string b64String= EncodeTo64(System.Text.ASCIIEncoding.ASCII.GetString(streamer.GetBuffer()));
+
+        Debug.Log(System.Text.ASCIIEncoding.ASCII.GetString(streamer.GetBuffer()));
+        Debug.Log(b64String);
+
+        byte[] writeBuffer = Convert.FromBase64String(b64String);
+
         //Save to disk
-        file.Write(streamer.GetBuffer(), 0, streamer.GetBuffer().Length);
+       // File.WriteAllText(fullSavePath,b64String);
+        File.WriteAllBytes(fullSavePath,writeBuffer);
+       // file.Write(writeBuffer, 0, writeBuffer.Length);
 
         // Close the file to prevent any corruptions
-        file.Close();
+      //  file.Close();
 
         string result = XElement.Parse(Encoding.ASCII.GetString(streamer.GetBuffer()).Replace("\0", "")).ToString();
-        Debug.Log("Serialized Result: " + result); */
+        //        Debug.Log("Serialized Result: " + result);
     }
 
     public static void SaveGameAsync(SaveData saveData)
     {
         Thread t = new Thread(() =>
         {
-            BinaryFormatter formatter = new BinaryFormatter();
+            /*             BinaryFormatter formatter = new BinaryFormatter();
 
-            FileStream stream = new FileStream(fullSavePath, FileMode.Create);
+                        FileStream stream = new FileStream(fullSavePath, FileMode.Create);
 
-            formatter.Serialize(stream, saveData);
-            stream.Close();
+                        formatter.Serialize(stream, saveData);
+                        stream.Close(); */
+
+            FileStream file = File.Create(fullSavePath);
+
+            DataContractSerializer bf = new DataContractSerializer(saveData.GetType());
+            MemoryStream streamer = new MemoryStream();
+
+            //Serialize the file
+            bf.WriteObject(streamer, saveData);
+            streamer.Seek(0, SeekOrigin.Begin);
+
+            //bf.ReadObject()
+
+
+            byte[] writeBuffer = EncodeTo64(streamer.GetBuffer());
+            //System.Text.ASCIIEncoding.ASCII.GetBytes(System.Convert.ToBase64String(streamer.GetBuffer()));
+
+            //Save to disk
+            file.Write(writeBuffer, 0, writeBuffer.Length);
+
+            // Close the file to prevent any corruptions
+            file.Close();
         });
 
         t.Start();
@@ -56,13 +91,53 @@ public static class SaveSystem
 
     public static SaveData LoadGame()
     {
+        /*         if (File.Exists(fullSavePath))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    FileStream stream = new FileStream(fullSavePath, FileMode.Open);
+
+                    SaveData saveData = formatter.Deserialize(stream) as SaveData;
+                    stream.Close();
+
+                    return saveData;
+                }
+                else
+                {
+                    Debug.LogError("Save file not found in " + fullSavePath);
+
+                    return null;
+                } */
+
         if (File.Exists(fullSavePath))
         {
-            BinaryFormatter formatter = new BinaryFormatter();
-            FileStream stream = new FileStream(fullSavePath, FileMode.Open);
+            // BinaryFormatter formatter = new BinaryFormatter();
+            FileStream file = new FileStream(fullSavePath, FileMode.Open);
 
-            SaveData saveData = formatter.Deserialize(stream) as SaveData;
-            stream.Close();
+            MemoryStream streamer = new MemoryStream((int)file.Length);
+
+            // streamer.
+
+            byte[] bytes = new byte[file.Length];
+
+            file.Read(bytes, 0, (int)file.Length);
+
+            //string b64String= DecodeFrom64(Convert.ToBase64String(bytes));
+            string str= System.Text.ASCIIEncoding.ASCII.GetString(bytes);
+            byte[] readBytes= Convert.FromBase64String(str);
+
+           // byte[] readBytes=DecodeFrom64(bytes);
+            streamer.Write(readBytes, 0, readBytes.Length);
+            DataContractSerializer bf = new DataContractSerializer(typeof(SaveData));
+
+            streamer.Seek(0, SeekOrigin.Begin);
+
+            //Debug.Log(streamer.Length);
+
+            // bf.ReadObject();
+
+            SaveData saveData = (SaveData)bf.ReadObject(streamer);
+
+            file.Close();
 
             return saveData;
         }
@@ -73,6 +148,49 @@ public static class SaveSystem
             return null;
         }
 
+    }
+
+    static public string EncodeTo64(string toEncode)
+    {
+        byte[] toEncodeAsBytes
+              = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+        string returnValue
+              = System.Convert.ToBase64String(toEncodeAsBytes);
+        return returnValue;
+    }
+
+
+
+    static public string DecodeFrom64(string encodedData)
+    {
+        byte[] encodedDataAsBytes
+            = System.Convert.FromBase64String(encodedData);
+        string returnValue =
+           System.Text.ASCIIEncoding.ASCII.GetString(encodedDataAsBytes);
+        return returnValue;
+    }
+
+    static public byte[] EncodeTo64(byte[] byteStringToEncode)
+    {
+        /*         byte[] toEncodeAsBytes
+                      = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode); */
+        string b64String
+              = System.Convert.ToBase64String(byteStringToEncode);
+        Debug.Log(b64String);
+        byte[] returnValue = System.Text.ASCIIEncoding.ASCII.GetBytes(b64String);
+        //byte[] returnValue = System.Convert.FromBase64String(b64String);
+        return returnValue;
+    }
+
+    static public byte[] DecodeFrom64(byte[] byteStringToDecode)
+    {
+        string b64String = System.Convert.ToBase64String(byteStringToDecode);
+        byte[] data=Convert.FromBase64String(b64String);
+        string decodedString=System.Text.ASCIIEncoding.ASCII.GetString(data);
+        Debug.Log(decodedString);
+        byte[] returnValue = System.Text.ASCIIEncoding.ASCII.GetBytes(decodedString);
+
+        return returnValue;
     }
 
     /*     public static void SaveGame(SaveData saveData)
